f491f98327b4db4d6d0d16b7070a75730581d7af
vfxnake <jazzjoe@gmail.com>
2022-09-30 21:52:27 -0400

    arror getter

--- src/chatgui.h
+++ src/chatgui.h
@@ -24,7 +24,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic->get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
